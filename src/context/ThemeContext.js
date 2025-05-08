d961e12e2bb49597f0dcd4a91a00cefe4fa53f83
Camila <cardenascastrolauracamila7@gmail.com>
2025-05-08 14:49:01 -0500

    feat: Context creation

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,11 @@
+import React, { createContext } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({children}) => {
+    return(
+        <ThemeContext.Provider value="test">
+            {children}
+        </ThemeContext.Provider>
+    )
+};
